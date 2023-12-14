49d61ee50b2e50109910009b602622f88cb1c0fa
Anthuyeen <87635968+Anthuyeen@users.noreply.github.com>
2023-12-14 12:05:37 +0700

    Reader

--- ReaderManager.xaml.cs
+++ ReaderManager.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Project_Library_Management_FA23_BL5.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Xps;
 
 namespace Project_Library_Management_FA23_BL5
 {
@@ -20,9 +22,108 @@ namespace Project_Library_Management_FA23_BL5
     /// </summary>
     public partial class ReaderManager : Page
     {
+        public LibraryManagementContext context;
         public ReaderManager()
         {
+            context = new LibraryManagementContext();
             InitializeComponent();
+            Load();
+        }
+
+        public Reader GetReader()
+        {
+            Reader reader = null;
+            try
+            {
+
+                reader = new Reader()
+                {
+                    //Id = int.Parse(txtEmployeeID.Text),
+                    CardNumber = int.Parse(txtCardNumber.Text)
+
+                };
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return reader;
+        }
+        //
+        //public static List<Reader> GetReaders()
+        //{
+        //    using var context = new LibraryManagementContext();
+        //    return context.Readers.ToList();
+        //}
+
+        //public static Reader? GetReader(int readerCardId)
+        //{
+        //    using var context = new LibraryManagementContext();
+        //    return context.Readers.FirstOrDefault(x => x.CardNumber == readerCardId);
+        //}
+
+        //public static Reader? GetReader(string fullname)
+        //{
+        //    using var context = new LibraryManagementContext();
+        //    return context.Readers.FirstOrDefault(x => x.FullName.Contains(fullname, StringComparison.OrdinalIgnoreCase));
+        //}
+
+        //public static void AddReader(Reader data)
+        //{
+        //    using var context = new LibraryManagementContext();
+        //    context.Readers.Add(data);
+        //    context.SaveChanges();
+        //}
+
+        //public static void UpdateReader(Reader data)
+        //{
+        //    using var context = new LibraryManagementContext();
+        //    context.Readers.Update(data);
+        //    context.SaveChanges();
+        //}
+
+        //public static void DeleteReader(Reader data)
+        //{
+        //    using var context = new LibraryManagementContext();
+        //    context.Readers.Remove(data);
+        //    context.SaveChanges();
+        //}
+        ////
+        public void Load()
+        {
+            try
+            {
+                using (var context = new LibraryManagementContext())
+                {
+                    var data = context.Readers.ToList();
+                    listReader.ItemsSource = data;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Button_Refresh_Click(object sender, RoutedEventArgs e)
+        {
+            
+            
+        }
+
+        private void btn_Add_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_Edit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_Delete_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
